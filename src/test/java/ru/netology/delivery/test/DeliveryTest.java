e08692d6d0f53354c3946590d96294f7dedc1cb0
Маргарита Мусатова <musatovamargarita@gmail.com>
2023-01-30 12:14:04 +0300

    Add Allure

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -2,9 +2,14 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.LogEventListener;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import io.qameta.allure.selenide.AllureSelenide;
 
 import java.time.Duration;
 
@@ -13,6 +18,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.delivery.data.DataGenerator.*;
 
 class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @BeforeEach
     void setup() {
@@ -43,6 +58,32 @@ class DeliveryTest {
         $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate2), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
+    }
+    @Test
+    void shouldBeNegativeChangeDate() {
+        $("[data-test-id='city'] input").setValue(generateCity("ru"));
+        String planningDate = generateDate(11);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input.input__control").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue(generateName("ru"));
+        $("[data-test-id='phone'] input.input__control").setValue(generatePhone("ru"));
+        $("[data-test-id='agreement'] ").click();
+        $("div>button").click();
+        $("[data-test-id='success-notification']").should(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+        String planningDate2 = generateDate(20);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input.input__control").setValue(planningDate2);
+        $("div>button").click();
+        $("[data-test-id='replan-notification']").should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='replan-notification'] .button").click();
+        $("[data-test-id='success-notification']").should(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно не запланирована на " + planningDate2), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
 
     }
